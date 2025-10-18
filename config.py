c70c2dfa9c1c6eca20568cbb6fa899d190a6d0c4
iShawyha <shawyhaf@gmail.com>
2025-10-19 01:01:51 +0300

    config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+settings = {
+    "width": 1200,
+    "height": 800,
+    "bg": "assets/background.png",
+    "player_images": "assets/Player",
+    "enemy_image": "assets/enemy.png",
+    "bonus_image": "assets/bonus.png",
+    "player_size": (182, 76),
+}
--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import pygame
+from config import settings
 from core.game import Game
-
-settings = {
-    "width": 1200,
-    "height": 800,
-    "bg": "assets/background.png",
-    "player_images": "assets/Player",
-    "enemy_image": "assets/enemy.png",
-    "bonus_image": "assets/bonus.png",
-    "player_size": (182, 76)
-}
 
 pygame.init()
 screen = pygame.display.set_mode((settings["width"], settings["height"]))
